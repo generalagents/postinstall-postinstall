b3140da36df319d16d09c239c874c1f2a5fcf2bf
Ulisse Mini <ulisse.mini@gmail.com>
2025-05-06 18:07:05 -0700

    log errors

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const cwd = require('process').cwd()
 const path = require('path')
-const exec = require('child_process').execSync
+const { execSync } = require('child_process')
 
 const appPath = path.normalize(cwd.slice(0, cwd.lastIndexOf('node_modules')))
 
@@ -12,13 +12,20 @@ if (fs.existsSync(packageJsonPath)) {
 
   if (packageJson.scripts && packageJson.scripts.postinstall) {
     const pkgManager = shouldUseYarn() ? 'yarn' : 'npm';
-    exec(`${pkgManager} run postinstall`, {cwd: appPath})
+    try {
+      execSync(`${pkgManager} run postinstall`, {cwd: appPath})
+    } catch (error) {
+      console.error('Postinstall failed:')
+      if (error.stdout) console.log(`stdout: ${error.stdout.toString()}`)
+      if (error.stderr) console.error(`stderr: ${error.stderr.toString()}`)
+      process.exit(1)
+    }
   }
 }
 
 function shouldUseYarn() {
   try {
-    exec('yarnpkg --version', { stdio: 'ignore' });
+    execSync('yarnpkg --version', { stdio: 'ignore' });
     return true;
   } catch (e) {
     return false;
